c111e925fbcaecde19f838afeee75856606c583f
dmitriy <dmitriy@localhost.localdomain>
2014-12-09 09:32:37 -0800

    added read op

--- src/java/org/justcodecs/dsd/ChunkDSTC.java
+++ src/java/org/justcodecs/dsd/ChunkDSTC.java
@@ -1,5 +1,12 @@
 package org.justcodecs.dsd;
 
-public class ChunkDSTC extends BaseChunk {
+import org.justcodecs.dsd.Decoder.DecodeException;
 
+public class ChunkDSTC extends BaseChunk {
+	@Override
+	void read(DSDStream ds) throws DecodeException {
+		super.read(ds);
+		
+		skip(ds);
+	}
 }
